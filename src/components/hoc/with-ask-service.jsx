8866b21815fb26ae97fdc3ad6089fc163600ed43
mikershov94 <ershovme@gmail.com>
2019-08-16 17:17:07 +0800

    COMMIT 2.3: Created HOC for context and data

---
+++ src/components/hoc/with-ask-service.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AskConsumer } from './../ask-service-context';
+
+const withAskService = (Wrapped) => {		//компонент высшего порядка
+	return (props) => {			//возвращаем функцию
+		return(								//которая возвращает компонент Consumer (достает данные из контекста)
+			<AskConsumer>
+				{										//определяем рендер-функцию
+					(askService) => {		//принимает из контекста API
+						return(						//возвращает компонент с данными
+							<Wrapped {...props}
+											 askService={ askService } />
+						);
+					}
+				}
+			</AskConsumer>
+		);
+	};
+};
+
+export default withAskService;

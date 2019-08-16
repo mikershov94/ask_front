2d28429898bd47046f5df2814aec4ea9126826e9
mikershov94 <ershovme@gmail.com>
2019-08-16 22:57:20 +0800

    COMMIT 8: Created component list

--- src/components/questions-list/questions-list.jsx
+++ src/components/questions-list/questions-list.jsx
@@ -1,63 +1,19 @@
 import React from 'react';
-
+import QuestionsListItem from './../questions-list-item';
 import "./questions-list.sass";
 
-const QuestionsList = () => {
+const QuestionsList = ({ questions }) => {
 	return(
 		<div className="questions-list">
-			
-			<div className="list-item">
-				<h3>Заголовок 1</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
-				<div className="meta-info">
-					<p>Задан: 01.07.2019</p>
-					<p>Лайков: 30</p>
-					<span>Нравится 
-						<i>
-							<img src="/icons/fill-like.svg" />
-						</i>
-						<i>
-							<img src="/icons/outline-like.svg" />
-						</i>
-					</span>
-				</div>
-			</div>
-
-			
-			<div className="list-item nechet">
-				<h3>Заголовок 2</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
-				<div className="meta-info">
-					<p>Задан: 02.07.2019</p>
-					<p>Лайков: 20</p>
-					<span>Нравится 
-						<i>
-							<img src="/icons/fill-like.svg" />
-						</i>
-						<i>
-							<img src="/icons/outline-like.svg" />
-						</i>
-					</span>
-				</div>
-			</div>
-		
-			
-			<div className="list-item">
-				<h3>Заголовок 3</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
-				<div className="meta-info">
-					<p>Задан: 03.07.2019</p>
-					<p>Лайков: 10</p>
-					<span>Нравится 
-						<i>
-							<img src="/icons/fill-like.svg" />
-						</i>
-						<i>
-							<img src="/icons/outline-like.svg" />
-						</i>
-					</span>
-				</div>
-			</div>
+			{
+				questions.map((question) => {
+					return(
+						<div key={question.id}>
+							<QuestionsListItem question={question} />							
+						</div>
+					);
+				})
+			}
 		</div>
 	);
 };

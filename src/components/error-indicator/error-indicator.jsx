6823afd3ec29780fccdf59e45abd919dcadca6e8
mikershov94 <ershovme@gmail.com>
2019-08-16 22:09:08 +0800

    COMMIT 3: Created error-indicator

---
+++ src/components/error-indicator/error-indicator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './error-indicator.sass';
+
+const ErrorIndicator = () => {
+	return(
+		<div>
+			<h2>Извините</h2>
+			<h3>Что-то пошло не так</h3>
+		</div>
+	);
+};
+
+export default ErrorIndicator;

e79c4eb016d7fbd9f5c29c0b8c92731e520fbb73
mikershov94 <ershovme@gmail.com>
2019-08-17 14:41:56 +0800

    Added actions for questions: REQUEST, SEUCCESS, FAILURE

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+const questionsRequested = () => {
+	return {
+		type: 'FETCH_QUESTIONS_REQUEST',
+	};
+};
+
+const questionsLoaded = (newQuestions) => {
+	return {
+		type: 'FETCH_QUESTIONS_SUCCESS',
+		payload: newQuestions
+	};
+};
+
+const errorLoading = (error) => {
+	return {
+		type: 'FETCH_QUESTIONS_FAILURE',
+		payload: error
+	};
+};
+
+export {
+	questionsRequested,
+	questionsLoaded,
+	errorLoading
+}

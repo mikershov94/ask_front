d78b12530df6c4825c1f819fe56ff8d491e87292
mikershov94 <ershovme@gmail.com>
2019-08-17 17:15:19 +0800

    Updated ask-service - added random call test error

--- src/services/ask-service.js
+++ src/services/ask-service.js
@@ -9,9 +9,13 @@ class AskService {
 	}
 
 	getQuestions() {
-		return new Promise((resolve) => {
+		return new Promise((resolve, reject) => {
 			setTimeout(() => {
-				resolve(this.data);
+				if (Math.random() < 0.75) {
+					resolve(this.data);
+				} else {
+					reject(new Error('Something happened!'));
+				};
 			}, 700);
 		});
 	}

70f48e2a5766827462095248952490e6260724e3
mikershov94 <ershovme@gmail.com>
2019-08-17 14:56:30 +0800

    Added React-Redux root components in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { AskProvider } from './components/ask-service-context';
+import ErrorBondary from '/components/error-bondary';
+import store from './store';
+import AskService from './services';
 import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const askService = new AskService();
+
+ReactDOM.render(<AskProvider value={askService} >
+									<ErrorBondary>
+										<Provider store={store} >
+											<App />
+										</Provider>
+									</ErrorBondary>
+								</AskProvider>, document.getElementById('root'));

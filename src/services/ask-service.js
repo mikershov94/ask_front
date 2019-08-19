16e843035ca7735c52e0075a0ceb0a1b3b17dc6e
mikershov94 <ershovme@gmail.com>
2019-08-19 14:14:40 +0800

    Add sort latest questions

--- src/services/ask-service.js
+++ src/services/ask-service.js
@@ -9,10 +9,21 @@ class AskService {
 	}
 
 	getLatestQuestions() {
+		const dateSort = (a, b) => {
+			if (a.date > b.date) {
+				return -1;
+			}
+			if (a.date < b.date) {
+				return 1;
+			}
+
+			return 0;
+		};
+
 		return new Promise((resolve, reject) => {
 			setTimeout(() => {
 				if (Math.random() < 0.75) {
-					resolve(this.data);
+					resolve(this.data.questions.sort(dateSort));
 				} else {
 					reject(new Error('Something happened!'));
 				};

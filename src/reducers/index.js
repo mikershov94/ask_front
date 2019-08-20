4aa4b360ebc487b88d55e9f4abe2f5647a4f1907
mikershov94 <ershovme@gmail.com>
2019-08-20 22:22:47 +0800

    Added reducers for sort actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,24 +13,42 @@ const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case 'FETCH_QUESTIONS_REQUEST':
 			return {
+				...state,
 				questions: state.questions,
 				loading: true,
 				errors: false
 			};
 		case 'FETCH_QUESTIONS_SUCCESS':
 			return {
+				...state,
 				questions: action.payload,
 				loading: false,
 				errors: false
 			};
 		case 'FETCH_QUESTIONS_FAILURE':
 			return {
+				...state,
 				questions: [],
 				loading: false,
 				errors: true
 			};
 
-		
+		case 'TOGGLE_SORT_TO_POPULAR':
+			return {
+				...state,
+				sidebar: {
+					latest: false,
+					popular: true
+				}
+			};
+		case 'TOGGLE_SORT_TO_LATEST':
+			return {
+				...state,
+				sidebar: {
+					latest: true,
+					popular: false,
+				}
+			};
 
 		default:
 			return state;

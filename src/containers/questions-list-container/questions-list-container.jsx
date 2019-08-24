49d62b2f8aff0247c1ae08a57c8e967352e389f3
mikershov94 <ershovme@gmail.com>
2019-08-24 16:31:22 +0800

    Created paginator. Styled paginator

--- src/containers/questions-list-container/questions-list-container.jsx
+++ src/containers/questions-list-container/questions-list-container.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Paginator from 'react-paginate'
 import { connect } from 'react-redux';
 import { compose } from './../../utils';
 import { withAskService } from './../../components/hoc';
@@ -24,6 +25,8 @@ class QuestionsListContainer extends React.Component {
 			errors
 		} = this.props;
 
+		const pageCount = Math.floor((page.count / 5) + 1)
+
 		if (loading) {
 			return <Spinner />;
 		};
@@ -32,7 +35,31 @@ class QuestionsListContainer extends React.Component {
 			return <ErrorIndicator />;
 		};
 
-		return <QuestionsList questions={page.results} />;
+		return (
+			<div>
+				<QuestionsList questions={page.results} />
+				<div className="d-flex justify-content-center">
+					<Paginator previousLabel={'Пред.'}
+					           nextLabel={'След.'}
+					           breakLabel={'...'}
+					           breakClassName={"break-me"}
+					           pageCount={pageCount}
+					           marginPagesDisplayed={2}
+					           pageRangeDisplayed={1}
+					           onPageChange={this.handlePageClick}
+					           containerClassName={"pagination"}
+					           breakClassName={"page-item"}
+					           breakLinkClassName={"page-link"}
+					           pageClassName={"page-item"}
+					           pageLinkClassName={"page-link"}
+					           previousClassName={"page-item"}
+					           previousLinkClassName={"page-link"}
+					           nextClassName={"page-item"}
+					           nextLinkClassName={"page-link"}
+					           activeClassName={"active"} />
+			</div>
+				</div>
+		);
 	}
 
 };

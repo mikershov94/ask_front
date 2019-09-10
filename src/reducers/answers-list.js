5dfda476a1683e6c085e674f8b91defff4cd42d9
mikershov94 <ershovme@gmail.com>
2019-09-10 21:34:53 +0800

    Add updateAnswers in reducer

---
+++ src/reducers/answers-list.js
@@ -0,0 +1,34 @@
+const updateAnswers = (state, action) => {
+
+	if (state === undefined) {
+		return {
+			data: [],
+			loading: true,
+			errors: false,
+		};
+	};
+
+	switch(action.type) {
+		case 'ANSWERS_LIST_REQUEST':
+			return {
+				data: [],
+				loading: true,
+				errors: false,
+			};
+		case 'ANSWERS_LIST_SUCCESS':
+			return {
+				data: action.payload,
+				loading: false,
+				errors: false,
+			};
+		case 'ANSWERS_LIST_FAILURE':
+			return {
+				data: [],
+				loading: false,
+				errors: true,
+			};
+		default:
+			return state.answers;
+	};
+
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import updateQuestionsList from './questions-list';
 import updateQuestionPage from './question-page';
 import updateSidebar from './sidebar';
+import updateAnswers from './answers-list';
 
 const reducer = (state, action) => {
 	return {
 		questionsList: updateQuestionsList(state, action),
 		questionPage: updateQuestionPage(state, action),
-		sidebar: updateSidebar(state, action)
+		sidebar: updateSidebar(state, action),
+		answers: updateAnswers(state, action),
 	};
 };
 
